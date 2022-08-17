26b1a6b67790ed665cbffca9bfa6b17e7ad9ae0f
crikke <crikke95@gmail.com>
2022-08-17 15:58:11 +0200

    test unmarshal mutation

--- pkg/commitlog/mutation_test.go
+++ pkg/commitlog/mutation_test.go
@@ -1,7 +1,34 @@
 package commitlog
 
-import "testing"
+import (
+	"encoding/binary"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestUnmarshalBinary(t *testing.T) {
+
+	data := make([]byte, 7)
+
+	binary.LittleEndian.PutUint16(data[0:2], uint16(3))
+	binary.LittleEndian.PutUint32(data[2:6], uint32(6))
+
+	data = append(data, []byte("foo")...)
+	data = append(data, []byte("barbaz")...)
+
+	m := &Mutation{}
+
+	err := m.UnmarshalBinary(data)
+
+	assert.Len(t, data, 16)
+	assert.NoError(t, err)
+
+	assert.Equal(t, []byte("foo"), m.Key)
+	assert.Equal(t, []byte("barbaz"), m.Value)
+	assert.Equal(t, false, m.tombstone)
+
+	assert.Equal(t, uint16(3), m.keyLength)
+	assert.Equal(t, uint32(6), m.valueLength)
 
 }
