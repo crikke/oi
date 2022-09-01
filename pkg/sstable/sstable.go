be9bf0264bf7e60b460e33d1794c25d8a5325249
big money stoffe <crikke95@gmail.com>
2022-09-01 09:21:50 +0200

    stuff

--- pkg/sstable/sstable.go
+++ pkg/sstable/sstable.go
@@ -3,7 +3,6 @@ package sstable
 import (
 	"bufio"
 	"crypto/md5"
-	"encoding/binary"
 	"errors"
 	"io"
 	"os"
@@ -11,6 +10,7 @@ import (
 
 	"github.com/crikke/oi/pkg/bloom"
 	"github.com/crikke/oi/pkg/memtree"
+	"github.com/crikke/oi/pkg/protoutil"
 	pb "github.com/crikke/oi/proto-gen/data"
 	"google.golang.org/protobuf/proto"
 )
@@ -25,56 +25,36 @@ import (
 // this means that if a sstable is renamed, the order is changed and the data is not valid
 // so this needs to be fixed later on
 
-
-type TableEntry struct {
-	r pb.Record
-}
-
-
 // index and summary have pretty much equal data structure
 
 // index depends on data file for position
-// summary depends on index for position 
-// 
+// summary depends on index for position
+//
 // Creation of new sstable:
 // create each file.
 // since SStable are immutable create or trunc existing files
-// 
+//
 // for each file create a chan
-// when inserted into data: put data offset and key into indexChannel 
+// when inserted into data: put data offset and key into indexChannel
 // when inserted into index: put index offset and key into summaryCh
-
-type protoEntry struct {
-	data []byte
-	dataLen uint32
-}
-
-func (p protoEntry) MarshalBinary() ([]byte, error) {
-
-	buf := make([]byte, p.dataLen + 4)
-	binary.LittleEndian.PutUint32(buf[0:4], p.dataLen)
-	buf = append(buf[4:], p.data...)
-
-	return buf, nil
-}
 
 type SSTable struct {
 	dir string
-	
-	entries int 
-	data appendOnlyFile
-	index appendOnlyFile
-	summary appendOnlyFile
-	done chan struct {}
+
+	entries    int
+	data       appendOnlyFile
+	index      appendOnlyFile
+	summary    appendOnlyFile
+	done       chan struct{}
 	sampleSize int
 }
 
 type appendOnlyFile struct {
-	w bufio.Writer
-	f *os.File
-	writerCh chan protoEntry
-	done chan struct{}
-	size uint32
+	w        *bufio.Writer
+	f        *os.File
+	writerCh chan protoutil.ProtoEntry
+	done     chan struct{}
+	size     uint32
 }
 
 func newAppendOnlyFile(path string, done chan struct{}) *appendOnlyFile {
@@ -90,88 +70,90 @@ func newAppendOnlyFile(path string, done chan struct{}) *appendOnlyFile {
 
 	aof.f = f
 	aof.w = bufio.NewWriter(f)
+
+	return aof
 }
 
-func (a* appendOnlyFile) append(p protoEntry) error {
+func (a *appendOnlyFile) append(p protoutil.ProtoEntry) error {
 
 	a.writerCh <- p
-	return nil 
+	return nil
 }
 
 func (a *appendOnlyFile) writerLoop() {
 
 	for {
-		select
-		{
-		case <- a.done:
+		select {
+		case <-a.done:
 			a.w.Flush()
 			a.f.Close()
 			return
-		case p := <-a.writerCh: 
+		case p := <-a.writerCh:
 			b, _ := p.MarshalBinary()
 			n, err := a.w.Write(b)
 			if err != nil {
 				panic(err)
 			}
 
-			a.size += n
+			a.size += uint32(n)
 		}
 	}
 }
 
 func NewSSTable(dir string) *SSTable {
 
-	s := *&SSTable{
+	s := &SSTable{
 		done: make(chan struct{}),
 	}
-	s.data = *newAppendOnlyFile(filepath.Join(dir, "data.db"), s.done)	
-	s.index = *newAppendOnlyFile(filepath.Join(dir, "index.db"), s.done)	
-	s.summary = *newAppendOnlyFile(filepath.Join(dir, "summary.db"), s.done)	
+	s.data = *newAppendOnlyFile(filepath.Join(dir, "data.db"), s.done)
+	s.index = *newAppendOnlyFile(filepath.Join(dir, "index.db"), s.done)
+	s.summary = *newAppendOnlyFile(filepath.Join(dir, "summary.db"), s.done)
+
+	return s
 }
 
-func (s *SSTable) Append(r pb.Record) error{
+func (s *SSTable) Append(r *pb.Record) error {
 	data, err := proto.Marshal(r)
 
 	if err != nil {
 		return err
 	}
 
-	p := protoEntry{
-		data:data,
-		dataLen: len(data),
+	p := protoutil.ProtoEntry{
+		Data:    data,
+		DataLen: uint32(len(data)),
 	}
-	
+
 	pos := s.data.size
 	if err := s.data.append(p); err != nil {
 		return err
 	}
-	
+
 	indexEntry := pb.IndexEntry{
-		Key:r.Key,
-		Position: pos,
+		Key:      r.Data.Key,
+		Position: uint64(pos),
 	}
-	
-	
-	data, err := proto.Marshal(&indexEntry)
+
+	data, err = proto.Marshal(&indexEntry)
 	if err != nil {
 		return err
 	}
 
-	p = protoEntry{
-		data:data,
-		dataLen: len(data),
+	p = protoutil.ProtoEntry{
+		Data:    data,
+		DataLen: uint32(len(data)),
 	}
 
 	if err := s.index.append(p); err != nil {
 		return err
 	}
-	
-	if s.entries % s.sampleSize == 0 {
+
+	if s.entries%s.sampleSize == 0 {
 		pos = s.index.size
 
 		summaryEntry := pb.IndexEntry{
-			Key:r.Key,
-			Position: pos,
+			Key:      r.Data.Key,
+			Position: uint64(pos),
 		}
 
 		data, err := proto.Marshal(&summaryEntry)
@@ -179,19 +161,19 @@ func (s *SSTable) Append(r pb.Record) error{
 			return err
 		}
 
-		p = protoEntry{
-			data:data,
-			dataLen: len(data),
+		p = protoutil.ProtoEntry{
+			Data:    data,
+			DataLen: uint32(len(data)),
 		}
 
 		if err := s.summary.append(p); err != nil {
 			return err
 		}
-	
 
 	}
 
 	s.entries++
+	return nil
 }
 
 func (s *SSTable) Done() error {
@@ -298,47 +280,6 @@ func checksum(r io.Reader) ([]byte, error) {
 	}
 	return hash.Sum(nil), nil
 }
-
-// creates a new SSTable at given path from a RBTree
-// The creation logic work by first creating the data file followed by the index file and then the summary file
-func New(dataDir string, m memtree.RBTree) error {
-
-	dirEntries, err := os.ReadDir(dataDir)
-	if err != nil {
-		return err
-	}
-
-	sstableDir := filepath.Join(dataDir, string(len(dirEntries)))
-
-	err = os.Mkdir(sstableDir, 0660)
-	if err != nil {
-		return err
-	}
-
-	// create data file
-	df, err := newDataFile(filepath.Join(sstableDir, "data.db"))
-	if err != nil {
-		return err
-	}
-
-	// append data to data file 
-	if err := df.appendRBtree(m); err != nil {
-		return err
-	}
-
-
-	if err := df.Close(); err != nil {
-		return err
-	}
-
-	// create index file from data file
-	if
-
-
-	return nil
-}
-
-// traverses the memtree and wrties the data to the files
 
 func (i *index) processNode(iw io.Writer, db io.Writer, n *memtree.Node) error {
 	l, err := db.Write(n.Value)
